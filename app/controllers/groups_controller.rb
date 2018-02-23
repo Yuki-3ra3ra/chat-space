919516c4880e50e0057915dae3b8084b048a23c9
佐々木雄紀 <sasakiyuki3ra3ra@gmail.com>
2018-02-23 17:17:14 +0900

    グループ編集機能の実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,6 +17,17 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+       render :edit
+    end
+  end
+
   private
   def group_params
   	params.require(:group).permit(:name, {:user_ids => [] })
