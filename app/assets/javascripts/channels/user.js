e4ee45d8ab6b61ad74e7bdb7b23100c6246cbe4f
佐々木雄紀 <sasakiyuki3ra3ra@gmail.com>
2018-02-27 18:34:55 +0900

    user#indexを記述

--- app/assets/javascripts/channels/user.js
+++ app/assets/javascripts/channels/user.js
@@ -1,6 +1,35 @@
 $(function() {
+  function buildSearchUserHTML(user){
+    var html = '<div class="chat-group-user clearfix">' +
+      '<p class="chat-group-user__name">' +
+      user.name +
+      '</p>' +
+      '<a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn" data-user-id="' + user.id + '" data-user-name="' + user.name + '">追加' +
+      '</a>' +
+      '</div>';
+    return html;
+  };
+
   $(".js-user-search-field").on("keyup", function() {
-    var input = $(".js-user-search-field").val();
-    console.log(input);
+    var name = $(".js-user-search-field").val();
+
+    $.ajax({
+      type: 'GET',
+      url: '/user',
+      data: {name: name },
+      dataType: 'json'
+    })
+    .done(function(users) {
+      var insertHTML = '';
+      users.forEach(function(user){
+        insertHTML += buildSearchedUserHTML(user);
+      });
+      $('.js-user-seaerch-result').empty();
+      $('.js-user-seaerch-result').append(insertHTML);
+    })
+
+    .fail(function() {
+      alert('ユーザーの検索に失敗しました');
+    });
   });
 });
