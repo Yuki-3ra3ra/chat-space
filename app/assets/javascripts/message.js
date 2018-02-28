d0f27de4e87980a4a80b7c69c04bc2dd65e8e47e
佐々木雄紀 <sasakiyuki3ra3ra@gmail.com>
2018-03-01 02:08:06 +0900

    jsに自動更新functionを追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -4,7 +4,7 @@ $(function(){
       ? ``
       : `<img src='${message.image}' , width = 250px, height = 150px >`
 
-    var html = `<div class = "message">
+    var html = `<div class = "message" data-id=${message.id}>
                  <div class = "upper-message">
                   <div class = "upper-message__user-name">
                     ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
                  </div>
                 </div>`
     return html;
-  }
+  };
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -48,4 +48,32 @@ $(function(){
       $('.form__submit').prop('disabled', false);
     });
   })
+
+  $(function(){
+    setInterval(update, 2000);
+  });
+  function update(){
+    var message_id = $(".message").last().data("id");
+    var url = location.href;
+    $.ajax({
+      url: location.href,
+      type: 'GET',
+      data: {id: message_id },
+      dataType: 'json'
+    })
+    .done(function(data){
+      console.log(data);
+      data.forEach(function(datum) {
+        var html = buildHTML(datum);
+        $(".messages").append(html)
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
+      })
+    })
+    .fail(function() {
+     alert('エラーのためメッセージの送信ができませんでした。');
+    })
+    .always(function() {
+      $('.form__submit').prop('disabled', false);
+    });
+  }
 });
